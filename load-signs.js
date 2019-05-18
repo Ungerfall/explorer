4d6b4b17a159872fb3a285642bbb37ecef48451a
epam <epam@EPRUIZHL0032.moscow.epam.com>
2019-05-18 15:37:23 +0400

    Pass interstringPlaces into renderMarkers

--- load-signs.js
+++ load-signs.js
@@ -1,5 +1,5 @@
-function renderMarkers(map) {
-    interestingPoints.forEach(el => {
+function renderMarkers(map, points) {
+    points.forEach(el => {
         DG.marker([el.lat, el.lon]).addTo(map).bindPopup(el.popup);
     });
 }
