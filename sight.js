ad4288a51b016ca465e17ce9798248ea2ceba0f8
ungerfall <ungerfall@gmail.com>
2019-05-19 01:28:47 +0400

    Add visited field

--- sight.js
+++ sight.js
@@ -1,14 +1,17 @@
 function renderMarkers(map, points) {
     points.forEach(el => {
-        var marker = DG.marker([el.lat, el.lon])
-            .addTo(map)
-            .bindPopup(el.popup);
-        el.id = marker;
+        if (!el.visited){
+            var marker = DG.marker([el.lat, el.lon])
+                .addTo(map)
+                .bindPopup(el.popup);
+            el.id = marker;
+        }
     });
 }
 
 function renderVisitedMarkers(map, points) {
     points.forEach(el => {
+        el.visited = true;
         el.id.removeFrom(map);
     });
 }
