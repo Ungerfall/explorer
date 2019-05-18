bae06d2cd2cebc9cd28b473f33f59438ab4c7160
Artem Maksimov <Artem_Maksimov@mail.ru>
2019-05-19 01:08:31 +0400

    Reformat code, fix infinity right request

--- nearest.js
+++ nearest.js
@@ -1,20 +1,13 @@
-function findNearestPoints(map, points, maxDistance) {
+function findNearestPoints(userLocation, points, maxDistance) {
     var nearestPoints = [];
 
-    map.locate()
-        .on('locationfound', function (userLocation) {
-            points.forEach(function (point) {
-                if (DG.latLng(point.lat, point.lon).distanceTo(DG.latLng(userLocation.latitude, userLocation.longitude)) <= maxDistance) {
-                    nearestPoints.push(point);
-                }
-            })
-        })
-        .on('locationerror', function (e) {
-            DG.popup()
-                .setLatLng(map.getCenter())
-                .setContent('Доступ к определению местоположения отключён')
-                .openOn(map);
-        });
+    points.forEach(function (point) {
+        if (DG.latLng(point.lat, point.lon).distanceTo(DG.latLng(userLocation.latitude, userLocation.longitude)) <= maxDistance) {
+            if (confirm("Вы уверены, что посетили " + point.popup + "?")) {
+                nearestPoints.push(point);
+            }
+        }
+    });
 
     return nearestPoints;
 }
