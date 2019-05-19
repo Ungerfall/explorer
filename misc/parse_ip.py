5b9ea889120a6e451de2097eaef65e5580cdf4d3
Dmitry Sutygin <dmitry.sutygin@gmail.com>
2019-05-19 10:12:14 +0400

    updated interesting places

--- misc/parse_ip.py
+++ misc/parse_ip.py
@@ -4,7 +4,7 @@ import xml.etree.ElementTree as ET
 def parse(files):
   js = open('ip.js', 'w')
   print('let interestingPoints = [', file=js)
-
+  id = 0
   for file in files:
     root = ET.parse(file).getroot()
     for type_tag in root.findall('node'):
@@ -21,7 +21,8 @@ def parse(files):
         if popup_name is None:
           popup_name = 'Интересный объект'
         if not bad_place:
-          print("{{ lat: {0}, lon: {1}, popup: \'{2}\' }},".format(lat, lon, popup_name), file=js)
+          print("{{ id: {3}, lat: {0}, lon: {1}, popup: \'{2}\', markerRef: null }},".format(lat, lon, popup_name, id), file=js)
+          id += 1
   
   print('];', file=js)
 
