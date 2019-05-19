9736ecf13f64e2334044b99aac70536d782f0bac
Dmitry Sutygin <dmitry.sutygin@gmail.com>
2019-05-19 08:13:26 +0400

    added login and sign up

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ window.addEventListener("load", function(e) {
 
     var authBtn = document.getElementById("btn-auth");
     authBtn.addEventListener("click",function(e){
-        authBtn.disabled = "true";
+        loginUserWithEmailAndPassword(login.value, password.value);
     },false);
 
     var mapBtn = document.getElementById("btn-map");
     mapBtn.addEventListener("click", function(e){
-        window.location.href = "map.html";
+        goToMap();
     })
 }, false);
 
@@ -29,4 +29,21 @@ function registerUserWithEmailAndPassword(email, pass) {
             var errorMessage = error.message;
             console.log(errorCode+errorMessage);
         });
+}
+
+function loginUserWithEmailAndPassword(email, pass) {
+    firebase.auth().signInWithEmailAndPassword(email, pass)
+    .then(function() {
+        goToMap();
+    })
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(error);
+      });
+}
+
+function goToMap() {
+    window.location.href = "map.html";
 }
